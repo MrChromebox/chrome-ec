a134ff8127bd2f3e652d3e588a7c8ffee7deb052
Duncan Laurie <dlaurie@google.com>
2017-05-16 07:37:33 -0700

    keyboard_8042: Allow scancode sets to be mutable
    Add an option to allow the scancode sets to be mutable.  The only
    reason to use this is to allow a scancode to be changed at runtime,
    for instance to support different keyboards in one image.
    
    The side effect of this is the scancode sets are moved out of the
    shared RO section.
    
    BUG=b:36735408
    BRANCH=none
    TEST=make -j buildall
    
    Change-Id: Iefb97691d1f295411d7b5db603d9214d41af49fd
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/506717
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    (cherry picked from commit e1696568c7cc9cfd6b55f4aa39620f52ef525f2a)

--- include/config.h
+++ include/config.h
@@ -1325,6 +1325,11 @@
 #undef CONFIG_KEYBOARD_PRINT_SCAN_TIMES
 
 /*
+ * Allow the keyboard scan code set tables to be modified at runtime.
+ */
+#undef CONFIG_KEYBOARD_SCANCODE_MUTABLE
+
+/*
  * Call board-supplied keyboard_suppress_noise() function when the debounced
  * keyboard state changes.  Some boards use this to send a signal to the audio
  * codec to suppress typing noise picked up by the microphone.
