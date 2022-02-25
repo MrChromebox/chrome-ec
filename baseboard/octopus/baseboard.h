8d0ec9d120b7968c17a12b2ee1412ff02e9b06fb
MrChromebox <mrchromebox@gmail.com>
2022-02-24 18:48:39 -0600

    baseboard/octopus: add vivaldi support
    Signed-off-by: MrChromebox <mrchromebox@gmail.com>
    (cherry picked from commit 4df1df2030cd469eedf19aa2e4ae9562b2bc614b)

--- baseboard/octopus/baseboard.h
+++ baseboard/octopus/baseboard.h
@@ -285,6 +285,7 @@
 #define CONFIG_KEYBOARD_PROTOCOL_8042
 #define CONFIG_KEYBOARD_COL2_INVERTED
 #define CONFIG_KEYBOARD_PWRBTN_ASSERTS_KSI2
+#define CONFIG_KEYBOARD_VIVALDI
 
 /*******************************************************************************
  * Sensor Config
