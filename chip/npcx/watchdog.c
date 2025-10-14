jackadam1981/ChromeEc

chip/npcx: Clear pending watchdog warning interrupt before enable
Clear watchdog warning timer interrupt (ITIM_WDG_NO) immediately before
enabling. The state of the watchdog warning timer is unpredictable after
a sysjump, so a watchdog warning timer interrupt may already be pending
when the interrupt is enabled during init. Clearing pending interrupt
before enabling is good hygine.

BUG=b:378342175
TEST=No watchdog warning after sysjump on quackingstick

Change-Id: Iab7a7b986debefe396601f7b5ec4958d372b4b4b

chip/npcx/watchdog.c
@@ -42,6 +42,8 @@ void watchdog_init_warning_timer(void)
 	NPCX_ITCNT(ITIM_WDG_NO) = CONFIG_AUX_TIMER_PERIOD_MS;
 	/* Event module enable */
 	SET_BIT(NPCX_ITCTS(ITIM_WDG_NO), NPCX_ITCTS_ITEN);
+	/* Clear pending interrupt before enabling */
+	task_clear_pending_irq(ITIM_INT(ITIM_WDG_NO));
 	/* Enable interrupt of ITIM */
 	task_enable_irq(ITIM_INT(ITIM_WDG_NO));
 }

MrChromebox/chrome-ec

before enabling is good hygiene.
BUG=b:378342175,b:445696855
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6442600
Tested-by: Boris Mittelberg <bmbm@google.com>
Reviewed-by: CH Lin <chlin56@nuvoton.com>
Commit-Queue: Boris Mittelberg <bmbm@google.com>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Reviewed-by: Boris Mittelberg <bmbm@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6961652
Reviewed-by: Jon Murphy <jpmurphy@google.com>
Tested-by: Rob Barnes <robbarnes@google.com>
Commit-Queue: Rob Barnes <robbarnes@google.com>
(cherry picked from commit faa25d7e63c89635fec64d735bac6a1ada85aab8)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7038842
Auto-Submit: Rob Barnes <robbarnes@google.com>
Owners-Override: Rob Barnes <robbarnes@google.com>
(cherry picked from commit 2e5b333a7591de9135e5b5a5ee599f4b65598162)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/7042883

chip/npcx/watchdog.c
@@ -42,6 +42,8 @@ void watchdog_init_warning_timer(void)
 	NPCX_ITCNT16(ITIM_WDG_NO) = CONFIG_AUX_TIMER_PERIOD_MS;
 	/* Event module enable */
 	SET_BIT(NPCX_ITCTS(ITIM_WDG_NO), NPCX_ITCTS_ITEN);
+	/* Clear pending interrupt before enabling */
+	task_clear_pending_irq(ITIM16_INT(ITIM_WDG_NO));
 	/* Enable interrupt of ITIM */
 	task_enable_irq(ITIM16_INT(ITIM_WDG_NO));
 }