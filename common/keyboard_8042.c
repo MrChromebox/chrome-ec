jackadam1981/ChromeEc

i8042: return self-test status on reset
As described in PS/2 keyboard protocol spec, the reset command 0xff
is followed by diagnostic self-test, aka BAT. Along with the ACK
for reset command EC must return BAT status.

BRANCH=none
BUG=b:134366527
TEST=buildall pass, TBD: test depthcharge screen

Change-Id: I5803d06b204d552c458ad443393f80072d41e40e
Signed-off-by: Boris Mittelberg <bmbm@google.com>

common/keyboard_8042.c
@@ -735,6 +735,7 @@ static int handle_keyboard_data(uint8_t data, uint8_t *output)
 			reset_rate_and_delay();
 			keyboard_clear_buffer();
 			output[out_len++] = ATKBD_RET_ACK;
+			output[out_len++] = ATKBD_RET_TEST_SUCCESS;
 			break;
 
 		case ATKBD_CMD_RESEND:

MrChromebox/chrome-ec

TEST=buildall; make run-kb_8042; depthcharge screen on brya
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4252007
Reviewed-by: Raul Rangel <rrangel@chromium.org>
Reviewed-by: CoolStar <coolstarorganization@gmail.com>
Tested-by: Raul Rangel <rrangel@chromium.org>
(cherry picked from commit 5f02b3e2f67c5f979f0209073194c887b28e6ba1)

common/keyboard_8042.c
@@ -701,6 +701,7 @@ static int handle_keyboard_data(uint8_t data, uint8_t *output)
 			reset_rate_and_delay();
 			keyboard_clear_buffer();
 			output[out_len++] = I8042_RET_ACK;
+			output[out_len++] = I8042_RESET_SELF_TEST;
 			break;
 
 		case I8042_CMD_RESEND: