jackadam1981/ChromeEc

Keyboard: Allow keyboard size to be set at run time
Currently, the keyboard size (i.e. number of columns) is static.

This patch allows it to be configured at run time. It's required to
support a keyboard with/without keypad in a single image.

Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

BUG=b:117126568
BRANCH=none
TEST=Verify keyboard functionality on Sona and Veyron.

Change-Id: I4b3552be0b4b315c3fe5a6884cf25e10aba8be7c

chip/stm32/clock-stm32l.c
@@ -281,7 +281,7 @@ void __enter_hibernate(uint32_t seconds, uint32_t microseconds)
 	 *
 	 * A little hacky to do this here.
 	 */
-	for (i = GPIO_KB_OUT00; i < GPIO_KB_OUT00 + KEYBOARD_COLS; i++)
+	for (i = GPIO_KB_OUT00; i < GPIO_KB_OUT00 + keyboard_cols; i++)
 		gpio_set_flags(i, GPIO_INPUT);
 
 	ccprints("fake hibernate. waits for power button/lid/RTC/AC");

MrChromebox/chrome-ec

KEYBOARD_COLS_MAX has the build time col size. It's used to allocate
exact spaces for arrays. Actual keyboard scanning is done using
keyboard_cols, which holds a runtime col size.
Reviewed-on: https://chromium-review.googlesource.com/1285292
Commit-Ready: Daisuke Nojiri <dnojiri@chromium.org>
Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
Reviewed-by: Randall Spangler <rspangler@chromium.org>
(cherry picked from commit 799e78da1e0917991fd29befc94af5d008097124)

chip/stm32/clock-stm32l.c
@@ -281,7 +281,7 @@ void __enter_hibernate(uint32_t seconds, uint32_t microseconds)
 	 *
 	 * A little hacky to do this here.
 	 */
-	for (i = GPIO_KB_OUT00; i < GPIO_KB_OUT00 + KEYBOARD_COLS; i++)
+	for (i = GPIO_KB_OUT00; i < GPIO_KB_OUT00 + KEYBOARD_COLS_MAX; i++)
 		gpio_set_flags(i, GPIO_INPUT);
 
 	ccprints("fake hibernate. waits for power button/lid/RTC/AC");