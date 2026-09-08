66aa85c1e83626fbfe637f4c0e06e564085c9981
Matt DeVillier <matt.devillier@gmail.com>
2026-09-08 15:34:56 -0500

    brya: default host sleep timeout to infinite
    Avoid EC hang recovery when Linux/Windows send timeout 0 (maps to the
    10s CONFIG_SLEEP_TIMEOUT_MS default) and s2idle is slow or stuck.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- baseboard/brya/baseboard.h
+++ baseboard/brya/baseboard.h
@@ -111,6 +111,8 @@
 #define CONFIG_POWER_S4_RESIDENCY
 #define CONFIG_POWER_SLEEP_FAILURE_DETECTION
 #define CONFIG_POWER_TRACK_HOST_SLEEP_STATE
+/* Host timeout 0 uses this; avoid 10s hang reset under Linux/Windows. */
+#define CONFIG_SLEEP_TIMEOUT_MS 0xFFFF /* infinite */
 #define CONFIG_LOW_POWER_IDLE
 
 #define CONFIG_HOST_INTERFACE_ESPI_RESET_SLP_SX_VW_ON_ESPI_RST
